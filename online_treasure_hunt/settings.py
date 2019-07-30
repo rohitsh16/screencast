eca1b5461537e455c99b75c9f126a1c9392806aa
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-07-30 08:37:15 +0530

    Fixes in settings.py

--- online_treasure_hunt/settings.py
+++ online_treasure_hunt/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', '1234#xyz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
